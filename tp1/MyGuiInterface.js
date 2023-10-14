de7555a2a79a648897526e615dfd84af3d32a11d
mafits <costamafalda7@gmail.com>
2023-10-14 18:32:48 +0100

    create shadow branch

--- tp1/MyGuiInterface.js
+++ tp1/MyGuiInterface.js
@@ -32,30 +32,16 @@ class MyGuiInterface  {
         // add a folder to the gui interface for the box
         const boxFolder = this.datgui.addFolder( 'Box' );
         // note that we are using a property from the contents object 
-        boxFolder.add(this.contents, 'boxMeshSize', 0, 10).name("size").onChange( () => { this.contents.rebuildBox() } );
-        boxFolder.add(this.contents, 'boxEnabled', true).name("enabled");
-        boxFolder.add(this.contents.boxDisplacement, 'x', -5, 5)
-        boxFolder.add(this.contents.boxDisplacement, 'y', -5, 5)
-        boxFolder.add(this.contents.boxDisplacement, 'z', -5, 5)
         boxFolder.open()
         
         const data = {  
             'diffuse color': this.contents.diffusePlaneColor,
             'specular color': this.contents.specularPlaneColor,
-            'color': this.contents.spotLight.color
+           // 'color': this.contents.spotLight.color
         };
-
-
-        const spotLightTargetPosition = {
-            'targetX' : this.contents.spotLight.target.position.x,
-            'targetY' : this.contents.spotLight.target.position.y
-        }
 
         // adds a folder to the gui interface for the plane
         const planeFolder = this.datgui.addFolder( 'Plane' );
-        planeFolder.addColor( data, 'diffuse color' ).onChange( (value) => { this.contents.updateDiffusePlaneColor(value) } );
-        planeFolder.addColor( data, 'specular color' ).onChange( (value) => { this.contents.updateSpecularPlaneColor(value) } );
-        planeFolder.add(this.contents, 'planeShininess', 0, 1000).name("shininess").onChange( (value) => { this.contents.updatePlaneShininess(value) } );
         planeFolder.open();
 
         // adds a folder to the gui interface for the camera
@@ -67,16 +53,6 @@ class MyGuiInterface  {
 
         // folder in the gui interface for the light
         const spotLightFolder = this.datgui.addFolder('SpotLight')
-        spotLightFolder.addColor(data, 'color').onChange( (value) => { this.contents.spotLight.color = value } );
-        spotLightFolder.add(this.contents.spotLight, 'intensity', 0, 20).onChange( (value) => { this.contents.spotLight.intensity = value } );
-        spotLightFolder.add(this.contents.spotLight, 'distance', 0, 15).onChange( (value) => { this.contents.spotLight.distance = value } );
-        spotLightFolder.add(this.contents.spotLight, 'angle',  0, 180).onChange( (value) => { this.contents.spotLight.angle = value*(Math.PI/180) } );
-        spotLightFolder.add(this.contents.spotLight, 'penumbra', 0, 2).onChange( (value) => { this.contents.spotLight.penumbra = value } );
-        spotLightFolder.add(this.contents.spotLight, 'decay', 0, 2).onChange( (value) => { this.contents.spotLight.decay = value } );
-        spotLightFolder.add(this.contents.spotLight.position, 'x', -20, 20).onChange( (value) => { this.contents.spotLight.position.x = value } );
-        spotLightFolder.add(this.contents.spotLight.position, 'y', -20, 20).onChange( (value) => { this.contents.spotLight.position.y = value } );
-        spotLightFolder.add(spotLightTargetPosition, 'targetX', -20, 20).onChange( (value) => { this.contents.updateSpotLightTargetX(value) } );
-        spotLightFolder.add(spotLightTargetPosition, 'targetY', -20, 20).onChange( (value) => { this.contents.updateSpotLightTargetY(value) } );
         spotLightFolder.open()
     }
 }
