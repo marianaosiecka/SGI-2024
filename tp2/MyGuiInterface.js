14f6a5c3ac604a0fe7e5bd11642102528e903aa5
Mafalda Costa <up202006417@up.pt>
2023-11-22 22:48:36 +0000

    add move outdoor lamp button

--- tp2/MyGuiInterface.js
+++ tp2/MyGuiInterface.js
@@ -6,7 +6,7 @@ import { MyContents } from './MyContents.js';
 /**
     This class customizes the gui interface for the app
 */
-class MyGuiInterface  {
+class MyGuiInterface {
 
     /**
      * 
@@ -14,7 +14,7 @@ class MyGuiInterface  {
      */
     constructor(app) {
         this.app = app
-        this.datgui =  new GUI();
+        this.datgui = new GUI();
         this.contents = null
         this.enableWireframe = false;
         this.enableExterior = true;
@@ -36,15 +36,31 @@ class MyGuiInterface  {
         // CAMERAS
         const cameraFolder = this.datgui.addFolder('Cameras')
         const cameraNames = Array.from(this.contents.cameras.keys());
-        cameraFolder.add(this.app, 'activeCameraName', cameraNames ).name("active camera");
+        cameraFolder.add(this.app, 'activeCameraName', cameraNames).name("active camera");
         cameraFolder.add(this.app.activeCamera.position, 'x', 0, 10).name("x coord")
         cameraFolder.close()
 
         // LIGHTS
         this.createLightsFolder();
 
+
+        let sceneFolder = this.datgui.addFolder('Scene')
+
+        // AXIS
+        sceneFolder.add(this, 'enableAxis')
+            .name('Axis')
+            .onChange(() => {
+                if (this.enableAxis) {
+                    this.contents.createAxis()
+                }
+                else {
+                    this.app.scene.remove(this.contents.axis);
+                    this.contents.axis = null;
+                }
+            });
+
         // WIREFRAME
-        this.datgui.add(this, 'enableWireframe')
+        sceneFolder.add(this, 'enableWireframe')
             .name('Wireframe')
             .onChange(() => {
                 this.contents.materials.forEach(material => {
@@ -55,47 +71,13 @@ class MyGuiInterface  {
                     }
                 });
             });
-        
 
-        // WINDOW
-        this.datgui.add(this.contents.nodes.get('windowDown').group.position, 'y', 0, 1.3).name("Open window");
-        
-        //DOOR 
-        this.datgui.add(this.contents.nodes.get('door').group.rotation, 'y', 0, 1.8).name("Open door");
-
-        // VINYL
-        this.createVinylPlayButton();
-
-        // SHADOWS
-        const shadowSlider = this.datgui.add({ shadowMapSize: 1024 }, 'shadowMapSize', 0, 4096).name('Shadow Map Size');
-
-        shadowSlider.onChange((value) => {
-            for (let light of this.contents.lights.values()) {
-                if (light.light.castShadow) {
-                    light.light.shadow.mapSize.width = value;
-                    light.light.shadow.mapSize.height = value;
-                }
-            }
-        });
-
-        // AXIS
-        this.datgui.add(this, 'enableAxis')
-            .name('Show axis')
-            .onChange(() => {
-                if(this.enableAxis){
-                    this.contents.createAxis()
-                }
-                else {
-                    this.app.scene.remove(this.contents.axis);
-                    this.contents.axis = null;
-                }
-            });
 
         // EXTERIOR
-        this.datgui.add(this, 'enableExterior')
-            .name('Show exterior walls')
+        sceneFolder.add(this, 'enableExterior')
+            .name('Exterior walls')
             .onChange(() => {
-                if(this.enableExterior){
+                if (this.enableExterior) {
                     this.showExteriorWalls()
                 }
                 else {
@@ -104,9 +86,58 @@ class MyGuiInterface  {
             });
 
 
+
+        // WINDOW
+        sceneFolder.add(this.contents.nodes.get('windowDown').group.position, 'y', 0, 1.3).name("Open window");
+
+        //DOOR 
+        sceneFolder.add(this.contents.nodes.get('door').group.rotation, 'y', 0, 1.8).name("Open door");
+
+        // VINYL
+        this.createVinylPlayButton(sceneFolder);
+
+        // OUTSIDE DOOR LAMP
+        this.createMoveOutsideDoorLampButton(sceneFolder);
+
+        // MIP MAPS
+        
+        this.options = {
+            minFilters: {
+                'NearestFilter': THREE.NearestFilter,
+                'NearestMipMapLinearFilter': THREE.NearestMipMapLinearFilter,
+                'NearestMipMapNearestFilter': THREE.NearestMipMapNearestFilter,
+                'LinearFilter ': THREE.LinearFilter,
+                'LinearMipMapLinearFilter (Default)': THREE.LinearMipMapLinearFilter,
+                'LinearMipmapNearestFilter': THREE.LinearMipmapNearestFilter,
+            },
+            magFilters: {
+                'NearestFilter': THREE.NearestFilter,
+                'LinearFilter (Default)': THREE.LinearFilter,
+            },
+        }
+
+        const mipmapsFolder = this.datgui.addFolder('Mipmaps');
+
+        for (const myTexture of this.contents.textures) {
+            const textureName = myTexture[0]
+            const texture = myTexture[1];
+            console.log(texture)
+            if (!(texture instanceof THREE.VideoTexture) && !texture.generateMipmaps) {
+                const textureFolder = mipmapsFolder.addFolder(textureName);
+        
+                const minFilterController = textureFolder.add(texture, 'minFilter', this.options.minFilters)
+                    .name('minFilter')
+                    .onChange((value) => this.updateMinFilter(value, textureName));
+        
+                const magFilterController = textureFolder.add(texture, 'magFilter', this.options.magFilters)
+                    .name('magFilter')
+                    .onChange((value) => this.updateMagFilter(value, textureName));
+    
+            }
+        }
     }
 
-    createLightsFolder(){
+    createLightsFolder() {
         const lightsFolder = this.datgui.addFolder('Lights');
         const lightTypesFolder = {};
 
@@ -168,7 +199,7 @@ class MyGuiInterface  {
         lightsFolder.close()
     }
 
-    createVinylPlayButton(){
+    createVinylPlayButton(sceneFolder) {
         const vinylNode = this.contents.nodes.get('vinyl').group;
         let isPlayingVinyl = false;
         let rotationCounter = 0;
@@ -176,7 +207,7 @@ class MyGuiInterface  {
 
         const videoElement = document.getElementById("music")
 
-        this.datgui.add({ playVinyl: () => playVinyl() }, 'playVinyl').name('Play Vinyl');
+        sceneFolder.add({ playVinyl: () => playVinyl() }, 'playVinyl').name('Play Vinyl');
 
         function playVinyl() {
             isPlayingVinyl = !isPlayingVinyl;
@@ -196,19 +227,18 @@ class MyGuiInterface  {
             if (isPlayingVinyl) {
                 vinylNode.rotation.y += 0.005;
                 if (zRotateUp) {
-                    vinylNode.rotation.z += 0.0004; 
-                } else vinylNode.rotation.z -= 0.0004; 
+                    vinylNode.rotation.z += 0.0004;
+                } else vinylNode.rotation.z -= 0.0004;
 
                 rotationCounter++;
-                if(rotationCounter % 150 == 0) zRotateUp = !zRotateUp
+                if (rotationCounter % 150 == 0) zRotateUp = !zRotateUp
 
                 requestAnimationFrame(rotateVinyl);
             }
         }
     }
 
-
-    showExteriorWalls(){
+    showExteriorWalls() {
         this.contents.nodes.get("exteriorWalls").group.visible = true
         this.contents.nodes.get("porch").group.visible = true
         this.contents.nodes.get("roof").group.visible = true
@@ -217,7 +247,7 @@ class MyGuiInterface  {
         this.contents.nodes.get("kite1").group.visible = true
     }
 
-    hideExteriorWalls(){
+    hideExteriorWalls() {
         this.contents.nodes.get("exteriorWalls").group.visible = false
         this.contents.nodes.get("porch").group.visible = false
         this.contents.nodes.get("roof").group.visible = false
@@ -225,6 +255,66 @@ class MyGuiInterface  {
         this.contents.nodes.get("chimney").group.visible = false
         this.contents.nodes.get("kite1").group.visible = false
     }
+
+    createMoveOutsideDoorLampButton(sceneFolder) {
+        let isMovingOutsideLamp = false;
+        sceneFolder.add({ moveOutsideLamp: () => moveOutsideLamp() }, 'moveOutsideLamp').name('Move Outside Lamp');
+        const lampNode = this.contents.nodes.get('outsideDoorLamp').group;
+
+        function moveOutsideLamp() {
+            if (isMovingOutsideLamp) {
+                return;
+            }
+            isMovingOutsideLamp = true;
+
+            const initialRotation = lampNode.rotation.x; // initial rotation
+            const initialPosition = lampNode.position.z // initial position
+            const oscillationFrequency = 2.5 * Math.PI / 3000; // speed
+            let oscillationAmplitude = 0.5; // moving range
+
+            const duration = 17000; // animation duration
+            const startTime = Date.now();
+
+            const animate = () => {
+                const elapsed = Date.now() - startTime;
+                const progress = Math.min(1, elapsed / duration);
+
+                // oscillate around the x-axis and adjust the position in z
+                const oscillation = Math.sin(oscillationFrequency * elapsed);
+                lampNode.rotation.x = initialRotation + oscillation * oscillationAmplitude;
+                lampNode.position.z = -(initialPosition + oscillation * oscillationAmplitude);
+
+                oscillationAmplitude *= 0.998; // lower the moving range in each rotation
+
+                // continue the animation until duration is reached or amplitude becomes very small
+                if (progress < 1 && oscillationAmplitude > 0.01) {
+                    requestAnimationFrame(animate);
+                }
+                else {
+                    isMovingOutsideLamp = false;
+                    lampNode.rotation.x = initialRotation;
+                    lampNode.position.z = initialPosition
+                }
+            };
+
+            animate();
+        }
+    }
+
+    // Function to update minFilter for a texture
+updateMinFilter(value, textureName) {
+    const texture = this.contents.textures[textureName];
+    texture.minFilter = value;
+    // Add any additional logic you need
+}
+
+// Function to update magFilter for a texture
+updateMagFilter(value, textureName) {
+    const texture = this.contents.textures[textureName];
+    texture.magFilter = value;
+    // Add any additional logic you need
+}
+
 
 
 }
